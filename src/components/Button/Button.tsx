6d0c18dae5578ff15479c4c998c2d7272c428a1b
Caique Oliveira <caique_m_oliveira@icloud.com>
2021-07-14 23:01:45 -0300

    chore: create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+/**
+ *
+ */
+const Button = styled.button`
+  border-radius: 31px;
+  background-image: linear-gradient(90deg, #859398 0%, #283048 100%);
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+  padding: 9px 22px;
+  color: #fff;
+  font-size: 1rem;
+  line-height: 1.1875rem;
+  font-weight: 700;
+  border: none;
+`;
+
+export default Button;
