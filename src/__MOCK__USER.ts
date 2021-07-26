c91454a5d021695a84cc4bbb944a3f831919ceec
Caique Oliveira <caique_m_oliveira@icloud.com>
2021-07-26 20:10:37 -0300

    chore: create mocked user

---
+++ src/__MOCK__USER.ts
@@ -0,0 +1,11 @@
+const USER = {
+  name: "Katrina Carolina Barreto",
+  profilePicture:
+    "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2F736x%2F00%2Fec%2F6b%2F00ec6b1a19a8dd9dee3949d4f7b09c1b.jpg&f=1&nofb=1",
+  storage: {
+    maxCapacity: 102400,
+    currentUsage: 7168,
+  },
+};
+
+export default USER;
