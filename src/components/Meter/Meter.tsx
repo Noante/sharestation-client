7972ecad487e9dd258d4cc53e7bc323a61612436
Caique Oliveira <caique_m_oliveira@icloud.com>
2021-07-26 20:22:42 -0300

    chore: create the Meter component

---
+++ src/components/Meter/Meter.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+const Bar = styled.meter`
+  height: 4px;
+  appearance: none;
+  background: none;
+  border-radius: 10px;
+  position: relative;
+  background-color: #fff;
+  &::-moz-meter-bar {
+    background: #283048;
+    border-radius: 10px;
+  }
+`;
+
+const Label = styled.span`
+  color: #283048;
+  font-size: 0.8125rem;
+  line-height: 1rem;
+`;
+
+const Labels = styled.p`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & > ${Bar} {
+    margin-bottom: 2px;
+  }
+`;
+
+type Props = {
+  currentValue: number;
+  max: number;
+  maxLabel: string;
+  minLabel: string;
+};
+
+/**
+ * Component fro representing a progress between two points.
+ * TODO: transform max and min labels into pseudo-elements.
+ */
+function Meter({ currentValue, max, maxLabel, minLabel }: Props) {
+  return (
+    <Wrapper>
+      <Bar max={max} value={currentValue}>
+        em {currentValue}/{max}
+      </Bar>
+      <Labels>
+        <Label>{minLabel}</Label>
+        <Label>{maxLabel}</Label>
+      </Labels>
+    </Wrapper>
+  );
+}
+
+export default Meter;
