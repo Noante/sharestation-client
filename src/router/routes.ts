237242e172b14789dc2e35bbb235073edda13032
Caique Oliveira <caique_m_oliveira@icloud.com>
2021-07-28 22:31:46 -0300

    chore: add files, file, and not-found routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,3 +2,10 @@ export const HOME = "/";
 export const RECENTS = `${HOME}recents`;
 export const RECOVERY = `${HOME}recovery`;
 export const STORAGE = `${HOME}storage`;
+
+// Files
+export const FILES = `${HOME}files`;
+export const FILE = `${FILES}/:fileId`;
+
+// Fallbacks
+export const NOT_FOUND = "*";
