ec5fba3f586ccdb11b79999af1f14658f818f191
Caique Oliveira <caique_m_oliveira@icloud.com>
2021-07-28 23:17:24 -0300

    chore: start the document view

---
+++ src/views/Document/Document.tsx
@@ -0,0 +1,15 @@
+import PrivateTemplate from "templates/PrivateTemplate";
+
+/**
+ * View responsible for displaying the available files inside
+ * the previous selected folder.
+ */
+function Document() {
+  return (
+    <PrivateTemplate>
+      <span>document view</span>
+    </PrivateTemplate>
+  );
+}
+
+export default Document;
