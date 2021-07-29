30a818151542dbdb6ae88f33a1ba7c2520eeda14
Caique Oliveira <caique_m_oliveira@icloud.com>
2021-07-28 23:18:55 -0300

    chore: start 404 view

---
+++ src/views/NotFound/NotFound.tsx
@@ -0,0 +1,9 @@
+/**
+ * View responsible for displaying the 404 error with some
+ * kind message.
+ */
+function NotFound() {
+  return <span>Oops... something is missing.</span>;
+}
+
+export default NotFound;
