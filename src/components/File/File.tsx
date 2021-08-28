abf8382227dc9dd859ba8877903dd7ebdd975c68
Caique Oliveira <caique_m_oliveira@icloud.com>
2021-08-28 14:49:51 -0300

    chore: create the `File` component

---
+++ src/components/File/File.tsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import getFileExtension from "utils/getFileExtension";
+import fileBackdropUrl from "./assets/fileBackdrop.png";
+import {
+  Slideshow,
+  Movie,
+  Image,
+  MusicNote,
+  MoreHoriz,
+} from "@styled-icons/material-outlined";
+import { StyledIconBase } from "@styled-icons/styled-icon";
+import getFileNameWithoutExtension from "utils/getFileNameWithoutExtension";
+
+const Backdrop = styled.figure`
+  position: relative;
+  text-align: center;
+  display: flex;
+  width: 96px;
+  height: 128px;
+  background-image: url(${fileBackdropUrl});
+
+  ${StyledIconBase} {
+    width: 31.5px;
+    height: 31.5px;
+    color: rgba(95, 106, 119, 0.5);
+    margin: auto;
+  }
+`;
+
+const Caption = styled.figcaption`
+  position: absolute;
+  top: calc(100% + 5px);
+`;
+
+const Wrapper = styled.div``;
+
+type Props = {
+  name: string;
+};
+
+/**
+ * Component for representing a file as a pdf, slideshow, video, picture, etc.
+ */
+function File({ name }: Props) {
+  const nameWithoutExtension = getFileNameWithoutExtension(name);
+
+  const icon = {
+    mp3: <MusicNote />,
+    ppt: <Slideshow />,
+    mp4: <Movie />,
+    jpg: <Image />,
+    other: <MoreHoriz />,
+  }[getFileExtension(name)];
+
+  return (
+    <Wrapper>
+      <Backdrop>
+        {icon}
+        <Caption>{nameWithoutExtension}</Caption>
+      </Backdrop>
+    </Wrapper>
+  );
+}
+
+export default File;
